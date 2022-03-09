610f3e3e3b97a24cfb6773b0d6ac9f6eb398ba97
Matthew Glassman <matthewglassman78@gmail.com>
2022-03-09 15:55:13 -0500

    Added a phrase to convert and an array of vowels.

---
+++ main.js
@@ -0,0 +1,5 @@
+//Phrase to convert to whale speake.
+let input = "turpentine and turtles";
+
+//Whales only speak in vowels so here is an array to hold those for converting purpose.
+var vowels = ["a", "e", "i", "o", "u"];
