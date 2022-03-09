7e379a3c3fa0d8b5448bc5801219df41db5a03ca
Matthew Glassman <matthewglassman78@gmail.com>
2022-03-09 16:07:04 -0500

    Add an empty array to hold converted phrase.  Added a for loop to iterate over the input variable.

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ let input = "turpentine and turtles";
 
 //Whales only speak in vowels so here is an array to hold those for converting purpose.
 var vowels = ["a", "e", "i", "o", "u"];
+
+//Where the actual converted phrase will be stored.
+let resultArray = [];
+
+//An array to iterate over each letter in the input variable.
+for (let i = 0; i < input.length; i++) {
+    const letter = input[i];
+    console.log(letter);
+}
